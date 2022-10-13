35a88d05c7405f248e92db5a0d3963436c9c3c30
Festus Omaku <fomaku3@gmail.com>
2022-10-13 16:08:52 -0700

    a little quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- this is the entry point for the program
+ *
+ *Return:1 if the function leads to an error, return not 1 if no error
+ *
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);

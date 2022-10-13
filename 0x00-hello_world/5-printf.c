73e9918278e214a214d81ef8bc78f49223d8b1d9
Festus Omaku <fomaku3@gmail.com>
2022-10-13 15:36:36 -0700

    betty again with the headache

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point for this program
+ **
+  * Return value - by default is 0 (success)
+ */
+int main(void)
+{
+	printf("%d\n", with proper grammar, but the outcome is a piece of art);
+		return (0);
+}

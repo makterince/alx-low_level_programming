3fa1d318ec5e6e54ad6b3f44bd654a9711fb709b
Festus Omaku <fomaku3@gmail.com>
2022-10-13 16:29:24 -0700

    hmm

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  *main- this is the entry point for the program
- *
  *Return:1 if the function leads to an error, return not 1 if no error
- *
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar,
 		2015-10-19\n", 59);
 	return (1);
+}

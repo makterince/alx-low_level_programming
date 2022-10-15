033014e1bef1da245c520c71e3bd5051bdf04f29
Festus Omaku <fomaku3@gmail.com>
2022-10-14 21:01:23 -0700

    0-9 with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

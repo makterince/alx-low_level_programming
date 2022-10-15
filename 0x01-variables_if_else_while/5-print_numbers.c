b0e84b138aa5891711279c3a36aad18954eed1e6
Festus Omaku <fomaku3@gmail.com>
2022-10-14 19:46:41 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}

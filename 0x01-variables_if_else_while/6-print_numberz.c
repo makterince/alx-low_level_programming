3e54d67549a99681196d1d0815c9281116f95c18
Festus Omaku <fomaku3@gmail.com>
2022-10-14 20:18:23 -0700

    almost same as lastr

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0 ; ch <= 10 ; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}

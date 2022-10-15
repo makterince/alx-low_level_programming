b2afdc47b7734515d16d5c6b15f8023db2c7aeaa
Festus Omaku <fomaku3@gmail.com>
2022-10-14 18:44:17 -0700

    lowcasing from 65 to 90

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}

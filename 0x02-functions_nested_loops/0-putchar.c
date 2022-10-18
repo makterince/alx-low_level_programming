4cfb8ccbb26e18d307d70a066c4e08a34854b81c
Festus Omaku <fomaku3@gmail.com>
2022-10-18 07:34:24 -0700

    program saying _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return:0 success
+ */
+
+int main(void)
+{
+	char word;
+
+	word = "_putchar";
+	printf("%s", word);
+	putchar('\n')
+
+		return (0);
+}

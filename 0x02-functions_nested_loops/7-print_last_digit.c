37e16c4ccd181b50fb1e3b8d63e98b6425e13b8b
Festus Omaku <fomaku3@gmail.com>
2022-10-18 15:14:51 -0700

    printing the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "math.h"
+/**
+ *print_last_digit - prints last digit of int
+ *Return: k the value of last digit
+ *@k: rep last digit formula
+ */
+
+int print_last_digit(int k)
+{
+	k = int % 10;
+	return (k);
+}

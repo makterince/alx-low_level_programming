abd9ebc42cc6d93593fb2beac9bce8f605331876
Festus Omaku <fomaku3@gmail.com>
2022-10-18 11:17:46 -0700

    lowercase alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints alphabets in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int num;
+
+	for (num = 'a'; num <= 'z'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}

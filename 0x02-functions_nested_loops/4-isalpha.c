385ee7fa3d43d963c584cbb654a7134c455f04e5
Festus Omaku <fomaku3@gmail.com>
2022-10-18 13:18:20 -0700

    check for lower and upper case

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_isalpha - checks for c in big or small letter
+ *@c:charachter
+ *Return:1 success return:0 failed
+ */
+
+int _isalpha(int c)
+{
+	return (c = 'a-z' && c = 'A-Z');
+}

f8e097d024e4fc584bcb69e1203f6a163fc61159
Festus Omaku <fomaku3@gmail.com>
2022-10-18 14:38:45 -0700

    print signs instead

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - prints signs for values gotten
+ *Return:1 if greater 0: if zero -1: if lesser
+ *@n: is the value checked
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+
+}

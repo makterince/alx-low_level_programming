6e5f6f53327c0d38c4f1ce56dd5dfc01f7c71c04
Festus Omaku <fomaku3@gmail.com>
2022-10-18 18:37:37 -0700

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *times_table - prints times table of 9 from 0
+ *
+ *
+ */
+void times_table(void)
+{
+	int num, multi, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multi = 1; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

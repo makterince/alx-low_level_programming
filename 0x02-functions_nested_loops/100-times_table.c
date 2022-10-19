4eebc76c6f5ff1466fa15626d5f85622ad67f3f9
Festus Omaku <fomaku3@gmail.com>
2022-10-19 03:36:17 -0700

    repair 3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,6 @@ void print_times_table(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		printf("%d, ", i);
-		_putchar('\n');
-	}
-	for (i = 0; i <= n; i++)
-	{
 		for (j = 0; j <= n; j++)
 		{
 			printf("%d", i * j);

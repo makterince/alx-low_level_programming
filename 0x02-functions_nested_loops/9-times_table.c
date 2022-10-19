fe2069537d01c80b1ad2ea78d1f38b9c1bef00f5
Festus Omaku <fomaku3@gmail.com>
2022-10-18 18:46:37 -0700

    repair2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,14 +16,14 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-			for (prod = 0; prod <= 9; prod++)
-			{
+
+			prod = num * multi;
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
 			_putchar((prod % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}

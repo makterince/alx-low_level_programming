815c6c8207c4fb50f7d88edc5fbfe0e12e3a8743
Festus Omaku <fomaku3@gmail.com>
2022-10-20 11:50:08 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *more_numbers - prints 1 to 14 in 10 rows
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar('a');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}

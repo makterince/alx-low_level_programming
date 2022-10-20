24af0b42a65e9917a8864b074876d56706e2799e
Festus Omaku <fomaku3@gmail.com>
2022-10-20 08:43:52 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "mian.h"
+/**
+ * _isupper - checks if c is upper case
+ *@c: integer
+ *Return: 1 upper 0 not
+ *
+ */
+int _isupper(int c)
+{
+	int c;
+
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

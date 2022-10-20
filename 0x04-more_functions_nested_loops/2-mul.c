0d9c494f568b2f90272213fcf0e728fbfcff84f3
Festus Omaku <fomaku3@gmail.com>
2022-10-20 09:40:15 -0700

    getting mul of two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - getsthe multiplication of two integers
+ *@a: integer 1
+ *@b: integer 2
+ *Return: the product
+ */
+int mul(int a, int b)
+{
+	return (int a * int b);
+}

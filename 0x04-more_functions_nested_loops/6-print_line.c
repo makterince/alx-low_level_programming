4037365216d72275c42049ceb30866f4d709072a
Festus Omaku <fomaku3@gmail.com>
2022-10-21 15:03:29 -0700

    fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,9 @@ void print_line(int n)
 {
 	int a;
 
-	for (a = 0; a <= n; a++)
+	for (a = 0; a < n; a++)
 	{
-		_putchar('*');
-		_putchar('\n');
-
-		if (n == 0)
-		{
-		_putchar('\n');
-		}
+		_putchar(95);
 	}
+	_putchar('\n');
 }

732f6e34dd96fae91877021b1bfa07c961c2c73a
Festus Omaku <fomaku3@gmail.com>
2022-10-21 14:28:26 -0700

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (b = 0; b <= 9; b++)
+	for (b = '0'; b <= '9'; b++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (a = '0'; a <= '14'; a++)
 		{
 			_putchar(a);
 		}

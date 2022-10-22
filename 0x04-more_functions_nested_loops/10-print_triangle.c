1d9df9eeb144febea04dcac62ea6a76589379a0d
Festus Omaku <fomaku3@gmail.com>
2022-10-21 18:51:09 -0700

    refixxx

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,19 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = size - i; j > 0; j--)
+			for (j = size - i; j > 1; j--)
 			{
-				if (j > i + 1)
-					_putchar(' ');
-				else
-					_putchar(35);
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
 			}
 			_putchar('\n');
 		}

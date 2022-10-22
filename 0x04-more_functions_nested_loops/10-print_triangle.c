46540ba31f23a90fe41d44096851430875ce90c8
Festus Omaku <fomaku3@gmail.com>
2022-10-21 18:38:54 -0700

    fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,29 @@
 #include "main.h"
 /**
- *
- *
+ *print_triangle - prints triangle in with hashtags
+ *@size: parameter
  *
  */
 void print_triangle(int size)
 {
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 0; j++)
+			{
+				if (j > i + 1)
+					_putchar(' ');
+				else
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }

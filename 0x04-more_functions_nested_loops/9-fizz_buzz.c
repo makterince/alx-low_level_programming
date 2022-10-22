dde3b86b6e0c18898ecf45917e17c4e6ddcd45d2
Festus Omaku <fomaku3@gmail.com>
2022-10-21 18:26:50 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
+ *main - prints value of 1 - 100 with fizz and buzz
+ *Return: 0 success
  *
  */
 int main(void)
 {
+	int i;
 
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+
+			if (i < 100)
+				printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
 }

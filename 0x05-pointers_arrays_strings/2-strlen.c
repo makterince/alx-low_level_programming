a7b53fcb16e69ee8ee7b7c15f3ce85e7a6c5fdcd
Festus Omaku <fomaku3@gmail.com>
2022-10-24 13:18:50 -0700

    _strlen task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: parameter
+ * Return: value of length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while
+	{
+		(str[len] != '\0')
+			len++
+	}
+	return len
+}

91dafb85d542fec7f5d1d8f5bbb614c8c4c60740
Festus Omaku <fomaku3@gmail.com>
2022-10-24 14:41:26 -0700

    4th task puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_puts - prints the string and ends with a new line
+ *@str: parameter
+ *
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

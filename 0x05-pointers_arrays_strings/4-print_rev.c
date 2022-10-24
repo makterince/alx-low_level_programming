4cae21b1fb196de6d4c211153d6f86aeb4fa1cd1
Festus Omaku <fomaku3@gmail.com>
2022-10-24 14:58:11 -0700

    5th task print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_rev - prints string in reverse
+ *@s: parameter
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	if (*s != '\0')
+	{
+		print_rev(s + 1);
+		_putchar(*s);
+	}
+	_putchar('\n');
+}

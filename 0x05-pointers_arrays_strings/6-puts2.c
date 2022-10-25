05e9d0beeabbcce55320266f9d061852c82d747d
Festus Omaku <fomaku3@gmail.com>
2022-10-24 19:11:16 -0700

    redo

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,19 +7,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (i >= 0)
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		if (str[i] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		i++;
-		}
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 }

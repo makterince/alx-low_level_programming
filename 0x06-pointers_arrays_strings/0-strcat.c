cc969075149147e3e3fac4f753d8f3f895a7e8c2
Festus Omaku <fomaku3@gmail.com>
2022-10-26 14:25:44 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <studio.h>
+/**
+ *_strcat - concatenates 2 string
+ *@dest: destination string
+ *@src: source string
+ *Return: pointer for destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	_strcat(s1, s2);
+	_putchar(s1);
+}

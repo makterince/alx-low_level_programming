dd8a258526c97f9d5a02d9d9f106973ad6b7617a
Festus Omaku <fomaku3@gmail.com>
2022-10-26 14:58:21 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,13 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 	{
-		for (j = 0; s2[j] != '\0'; j++, i++)
+		for (j = 0; src[j] != '\0'; j++, i++)
 		{
-			s1[i] = s2[j];
+			dest[i] = src[j];
 		}
-		s1[i] = '\0';
+		dest[i] = '\0';
 	}
+	return (dest);
 }

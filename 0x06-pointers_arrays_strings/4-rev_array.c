c725c82f3d8b4c91c2c08d321f9ecaab40cf1158
Festus Omaku <fomaku3@gmail.com>
2022-10-27 06:10:41 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *reverse_array - reverse the content of an array
+ *@a: parameter
+ *@n: parameter
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i,
+
+	for (i = n - 1; a[i] >= 0; i--)
+	{
+		;
+	}
+}

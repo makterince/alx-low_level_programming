f2bb8cd7557f673241bcb5122efa346b47f7bcc5
Festus Omaku <fomaku3@gmail.com>
2022-10-27 18:55:06 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *
- *
- *
+ *cap_string - capitalize anything possible
+ *@s: parameter
+ *Return: s
  *
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int i;
 
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' ' ||
+			s[i - 1] == '\t' ||
+			s[i - 1] == '\n' ||
+			s[i - 1] == '.' ||
+			s[i - 1] == ',' ||
+			s[i - 1] == ';' ||
+			s[i - 1] == '!' ||
+			s[i - 1] == '?' ||
+			s[i - 1] == '"' ||
+			s[i - 1] == '{' ||
+			s[i - 1] == '}' ||
+			s[i - 1] == '(' ||
+			s[i - 1] == ')' ||
+			i == 0)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
 }

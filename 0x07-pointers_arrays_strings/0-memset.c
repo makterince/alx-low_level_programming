f43552827c3a2391e10a0e31a6cb1ce6f88028d7
Festus Omaku <fomaku3@gmail.com>
2022-10-31 08:09:08 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_memset - fills memory with constant byte
+ *@s: parameter
+ *@b: parameter
+ *@n: amount of parameter intended
+ *Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i,
+
+	for (i = 0; i < 10; i++)
+		s[i] = b;
+	for (i = 0; 1 < 10; i++)
+		_putchar(s[i]);
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+working on more and more pointers and arrays and strings
+pointers to pointers and stuffs

2934b39784a9a6b58f6fecada1b1ae1760703622
Festus Omaku <fomaku3@gmail.com>
2022-10-31 17:37:52 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *set_string - sets the value of a points to another
+ *@s: double pointer
+ *@to: pointer
+ *
+ *
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

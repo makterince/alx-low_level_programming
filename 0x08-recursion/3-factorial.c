6f1aa713dfbe4da259259362a4bc7516f07aaee5
Festus Omaku <fomaku3@gmail.com>
2022-11-02 15:33:19 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *factorial - gets the factorial of a num
+ *@n: parameter
+ *Return: 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n = factorial(n - 1));
+	}
+}

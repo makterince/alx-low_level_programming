ef3ebfc73a03966a81783b4e39b3dd68e301e6af
Festus Omaku <fomaku3@gmail.com>
2022-11-02 13:58:16 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts_recursion - prints out a string
+ *@s: parameter
+ *Return: s
+ *
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	_putchar(i);
+	_putchar('\n');
+	return (s);
+}

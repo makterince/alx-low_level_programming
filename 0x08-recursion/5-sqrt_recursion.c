6e3484807307b1ffa06013af457120bb1f51cad0
Festus Omaku <fomaku3@gmail.com>
2022-11-02 16:50:15 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,11 +2,28 @@
 /**
  *_sqrt_recursion - gets the original sqrt of n
  *@n: parameter
- *
+ *Return: sqrt
  *
  *
  */
 int _sqrt_recursion(int n)
 {
-	
+	return (_sqrt(n, 1));
+}
+
+/**
+ *_sqrt - modified recusive function as original sqrt of n
+ *@n: parameter
+ *@i: parameter
+ *Return: sqr
+ */
+int _sqrt(int n, int i)
+{
+	int sq = i * i;
+
+	if (sq < n)
+		return (-1);
+	if (sq == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }

fa9f04bf2d2fd747919e72ab4ec64d4088aefb20
Festus Omaku <fomaku3@gmail.com>
2022-11-02 14:28:37 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,10 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		return;
+		_puts_recursion(*s++);
 	}
 	else
 	{
-		_putchar(*s);
-		_putchar(*s++);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

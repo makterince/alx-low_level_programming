0cab4814bc513d4741b660b0b3b2010ab611a387
Festus Omaku <fomaku3@gmail.com>
2022-11-02 15:57:15 -0700

    task 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_pow_recursion - gives the power of y to x
+ *@x: parameter
+ *@y: parameter
+ *Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y >= 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}

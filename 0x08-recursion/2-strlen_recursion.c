90e1b761b4d31b65e48d22495b28789cdac160a0
Festus Omaku <fomaku3@gmail.com>
2022-11-02 15:24:32 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,14 +9,11 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s != '\0')
+	if (*s)
 	{
 		len++;
-		_strlen_recursion(++s);
+		len = len + _strlen_recursion(s + 1);
 	}
-	else
-	{
-		return (len);
-	}
+
 }
 

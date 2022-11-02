a2a49e7bd0ea91104ab3edd747954ffe890eec05
Festus Omaku <fomaku3@gmail.com>
2022-11-02 15:01:39 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strlen_recursion - returns the length of a string
+ *@s: parameter
+Return: len*
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	static int len;
+
+	len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		_strlen_recursion(++s);
+	}
+	else
+	{
+		return (len);
+	}
+}

fa793b469bd583e9362937b3c2dbb09544ffbed6
Festus Omaku <fomaku3@gmail.com>
2022-11-04 00:58:47 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,13 +5,9 @@
  *@argv: parameter
  *Return: 0 success
  */
-int main(int argc, char argv)
+int main(int argc, char *argv[])
 {
-	int i;
+	(void)argc;
 
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s ", argv[i]);
-		}
-		printf("\n");
+	printf("%s\n", argv[0]);
 }

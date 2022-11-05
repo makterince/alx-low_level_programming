a6d7c5fd622c1a804897aa83e2ee35692ecebe64
Festus Omaku <fomaku3@gmail.com>
2022-11-04 18:22:07 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,13 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	
+	int i = 0;
+
 	if (argc > 0)
 	{
-		for (i = 0; i < argc; i++)
+		while (i < argc)
 		{
-			printf("%s\n", argv[i])
+			printf("%s\n", argv[i]);
+			i++;
 		}
 	}
 	return (0);

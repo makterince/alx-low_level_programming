997c4535a4e7cd4a09f371107513fff954cfefa8
Festus Omaku <fomaku3@gmail.com>
2022-11-04 17:21:00 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  *main - returns numbers of arguments passed when running prog
@@ -8,5 +9,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("%d\n", argc);
+	printfu("%d\n", argc);
+	return (0);
 }

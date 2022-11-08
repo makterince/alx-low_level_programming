9aa8d9695efea2893825db1cd5340fdc93e64ee2
Festus Omaku <fomaku3@gmail.com>
2022-11-08 09:34:45 -0800

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,48 +1,4 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
-
-/**
- *_strcat - concatenatthe string labled src and merges it with dest
- *@dest: destination parameter
- *@src: source parameter
- *Return: dest
- */
-char *_strcat(char *dest, char *src)
-{
-	int i, j;
-
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- *_strlen - tells the size of str
- *@s: parameter
- *Return: i
- */
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 /**
  *str_concat - concatenates two strings
  *@s1: parameter
@@ -59,13 +15,13 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	size = (_strlen(s1) + _strlen(s2) + 1);
+	size = (strlen(s1) + strlen(s2) + 1);
 	p = (char *) malloc(size * sizeof(char));
 	if (p == 0)
 	{
 		return (NULL);
 	}
-	p = _strcat(s1, s2);
+	p = strcat(s1, s2);
 	return (p);
 }
 

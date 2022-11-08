806d767499383073ebd91320537fe6db1be11e75
Festus Omaku <fomaku3@gmail.com>
2022-11-08 13:57:58 -0800

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ *argstostr - concats all arguments passed
+ *@ac: parameter
+ *@av: parameter
+ *Return: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, l;
+	char *p;
+
+	if (ac == 0 || av == 0)
+	{
+		return (NULL);
+	}
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			k++;
+			j++;
+		}
+		i++;
+	}
+	k = k + ac + 1;
+	p = malloc(sizeof(char) * k);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			p[l] = av[i][j];
+			l++;
+		}
+		p[l] = '\n';
+		l++;
+	}
+	return (p);
+}

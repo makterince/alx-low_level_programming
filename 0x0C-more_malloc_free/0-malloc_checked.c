ab6485c4df74ff42c8a4e185440ad69a890340f8
Festus Omaku <fomaku3@gmail.com>
2022-11-10 18:58:28 +0100

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
- *
- *
- *
+ *malloc_checked - checks if memory is being allocated
+ *@b: parameter
+ *Return: 0
  *
  */
 void *malloc_checked(unsigned int b)
 {
-	
+	char *p;
+
+	*p = (char *)malloc(sizeof(char) * int);
+	if (ptr == NULL)
+	{
+		return (98);
+	}
+	return (p);
 }

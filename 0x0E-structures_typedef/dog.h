c784c5948cc22a8af1644b8b7166144e0b59d795
Festus Omaku <fomaku3@gmail.com>
2022-11-14 13:32:47 +0100

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
 /**
  * struct dog - for dog
  * @name: parameter
  * @age: parameter
  * @owner: parameter
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-dog dog_t;
+/**
+ * dog_t - new
+ */
+typedef dog dog_t;
 #endif

00bab9da22072566b7ceb9d014618ac125dc0065
Festus Omaku <fomaku3@gmail.com>
2022-11-14 15:05:55 +0100

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,17 +16,17 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	}
 	else
-		printf("%s\n", d->name);
+		printf("Name: %s\n", d->name);
 	if (d-> age == 0)
 	{
 		printf("Age: 0\n");
 	}
 	else
-		printf("%f\n", d->age);
+		printf("Age: %f\n", d->age);
 	if (d-> owner == NULL)
 	{
 		printf("Owner: nil\n");
 	}
 	else
-		printf("%s\n", d->owner);
+		printf("Owner: %s\n", d->owner);
 }

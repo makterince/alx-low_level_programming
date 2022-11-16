e085f95baa010214d5f4306040ba702fa59dcd8c
Festus Omaku <fomaku3@gmail.com>
2022-11-16 15:16:17 +0100

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include "3-calc.h"
+/**
+ *main - performs simple operation
+ *@argc: parameter
+ *@argv: parameter
+ *Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int arg1;
+	int arg2;
+	int result;
+	char p;
+	int (*func)(int,int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	 arg1 = atoi(argv[1]);
+	 arg2 = atoi(argv[3]);
+
+	 func = get_op_func(argv[2]);
+
+	 if (!func)
+	 {
+		printf("Error\n");
+		exit(99);
+	 }
+	 p = *argv[2];
+	 if ((p == '/' || p == '%') && arg2 == 0)
+	 {
+		 printf("Error\n");
+		 exit(100);
+	 }
+	 result = func(arg1, arg2);
+	 printf("%d\n", result);
+	 return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ *op_add - adds a and b
+ *@a: parameter
+ *@b: parameter
+ *Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - minus a from b
+ *@a: parameter
+ *@b: parameter
+ *Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - times a and b
+ *@a: parameter
+ *@b: parameter
+ *Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divides a and b
+ *@a: parameter
+ *@b: parameter
+ *Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - gets mod of a and b
+ *@a: parameter
+ *@b: parameter
+ *Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+

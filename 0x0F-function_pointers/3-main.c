58fd88e193ad6d64104585608fe85d76cfe974b2
Festus Omaku <fomaku3@gmail.com>
2022-11-16 15:22:26 +0100

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 	int arg2;
 	int result;
 	char p;
-	int (*func)(int,int);
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -24,18 +24,18 @@ int main(int argc, char *argv[])
 
 	 func = get_op_func(argv[2]);
 
-	 if (!func)
-	 {
+	if (!func)
+	{
 		printf("Error\n");
 		exit(99);
-	 }
-	 p = *argv[2];
-	 if ((p == '/' || p == '%') && arg2 == 0)
-	 {
-		 printf("Error\n");
-		 exit(100);
-	 }
-	 result = func(arg1, arg2);
-	 printf("%d\n", result);
-	 return (0);
+	}
+	p = *argv[2];
+	if ((p == '/' || p == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = func(arg1, arg2);
+	printf("%d\n", result);
+	return (0);
 }

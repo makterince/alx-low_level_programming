94babb24040cf14fcec78ff601fe83ed22584a71
Festus Omaku <fomaku3@gmail.com>
2022-11-24 20:27:47 +0100

    task 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,11 +18,9 @@ size_t print_list(const list_t *h)
 			h = h->next;
 		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
-		i++;
+		h = h->next;
+		i++
 	}
 	return (i);
 }

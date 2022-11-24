2a1cc073e1f8c6715e7eb9999deb153d0682a2d1
Festus Omaku <fomaku3@gmail.com>
2022-11-24 17:50:24 +0100

    task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+/**
+ *add_node_end - adds a node to the end of link
+ *@head: pointer to pointer to head
+ *@str: string parameter
+ *Return: new node
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	struct list_t *new_node, *current;
+	unsigned int length;
+	char *cpy;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	cpy = strdup(str);
+	if (str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	for (length = 0; str[length] != '\0'; length++)
+		;
+
+	new_node->str = cpy;
+	new_node->len = length;
+	new_node->next = NULL;
+
+	*current = *head;
+	if (*head == NULL)
+		*head = *new_node;
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (*head);
+}

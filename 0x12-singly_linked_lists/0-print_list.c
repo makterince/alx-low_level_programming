5c659383dea2654ea6966e43718d1dcdfd4bd0b9
Festus Omaku <fomaku3@gmail.com>
2022-11-24 15:38:36 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,18 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
+	if (h->str == NULL)
+	{
+		printf("(nil");
+		h = h->next;
+	}
+	else if (h->len == 0)
+	{
+		printf("[0]");
+		h = h->next;
+	}
 	for (i = 0; h != NULL; i++)
 	{
-		if (h->str == NULL)
-		{
-			printf("(nil");
-			h = h->next;
-		}
-		else if (h->len == 0)
-		{
-			printf("[0]");
-			h = h->next;
-		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}

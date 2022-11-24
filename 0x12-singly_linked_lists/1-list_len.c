4ebf6c9809bdea082bfce2af3c2753babb36eee7
Festus Omaku <fomaku3@gmail.com>
2022-11-24 16:07:19 +0100

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,11 +10,11 @@ size_t list_len(const list_t *h)
 	size_t i;
 
 	i = 0;
-	 while (h != NULL)
-	 {
-		 i++;
-		 h = h->next;
-	 }
-	 return (i);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
 

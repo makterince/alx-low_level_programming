69e624e6755f86a6fd3fcfe7c9a7536cbf72e1f7
Festus Omaku <fomaku3@gmail.com>
2022-11-29 00:59:24 +0100

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,8 +3,28 @@
  *add_nodeint_end - adds a node at the end
  *@n: parameter
  *Return: temp address or null
- *
+ *@head: head pointer for first member of link
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *temp, *end;
 
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+
+	temp->n = n;
+	temp->next = NULL;
+	if (*head == NULL)
+		*head = temp;
+	else
+	{
+		end = *head;
+		while (end->next != NULL)
+		{
+			end = end->next;
+		}
+		end->next = temp;
+	}
+	return (*head);
+}

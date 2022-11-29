235d206756e14b16c596d775f3f5eb44d50fa1cb
Festus Omaku <fomaku3@gmail.com>
2022-11-29 19:56:01 +0100

    task 9/10

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	anywhere->n = n;
 
-	if (idx < 1)
-		return (NULL);
 	if (idx == 0)
 	{
 		anywhere->next = drop;

121f0aa2c37401c3a0bda5951fabc27b184dd035
Festus Omaku <fomaku3@gmail.com>
2022-11-29 01:10:22 +0100

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,9 +2,17 @@
 /**
  *free_listint - frees listint
  *Return: nothing
- *
+ *@head: head pointer
  *
  */
 void free_listint(listint_t *head)
 {
+	listint_t *temp;
 
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}

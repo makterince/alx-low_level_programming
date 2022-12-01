8600fedfb38c2e6b68235a75d705e1af23e56591
Festus Omaku <fomaku3@gmail.com>
2022-12-01 16:32:50 +0100

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *binary_to_uint - converts binary to an unsigned integer
+ *@b: binary number
+ *Return: number
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int bin;
+	unsigned int value;
+
+	if (b == '\0')
+		return (0);
+	if (b != '\0')
+	{
+		for (bin = 1; b[bin] != '\0'; bin++)
+		{
+			if (b[bin] < '0' || b[bin] > '1')
+				return (0);
+			value = 2 * value + (b[bin] - '0');
+		}
+	}
+	return (value);
+}

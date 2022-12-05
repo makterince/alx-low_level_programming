088f72e32c81850b5b2d66499e27ae08dcbd7510
Festus Omaku <fomaku3@gmail.com>
2022-12-05 23:46:23 +0100

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *append_text_to_file - appends a text
+ *@filename: name
+ *@text_content: content
+ *Return: 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, length;
+
+	length = 0;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, length);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}

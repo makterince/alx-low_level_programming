789343c32c969cb3fa869918a9617d34af49911a
Festus Omaku <fomaku3@gmail.com>
2022-12-06 00:20:47 +0100

    from and to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -43,7 +43,7 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int initial, dest, r, w;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
@@ -52,31 +52,31 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	buffer = create_buffer(argv[2]);
-	initial = open(argv[1], O_RDONLY);
-	r = read(initial, buffer, 1024);
-	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
-		if (initial == -1 || r == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
-		w = write(dest, buffer, r);
-		if (dest == -1 || w == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
-		r = read(initial, buffer, 1024);
-		dest = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
-	close_file(initial);
-	close_file(dest);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }

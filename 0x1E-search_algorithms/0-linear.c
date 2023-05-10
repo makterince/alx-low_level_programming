173e50ea1a322bc7f05da27328491cd35c746580
Festus Omaku <fomaku3@gmail.com>
2023-05-10 14:16:49 +0100

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,12 +9,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array == NULL) {
 		return -1;
 	}
 	
-	for (size_t i = 0; i < size; i++) {
-		printf("Comparing %d\n", array[i]);
+	for (i = 0; i < size; i++) {
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value) {
 			return i;  // Found the value at index i
 		}

3857a43f66a52a650a321990fe2ce512fd4745b1
Festus Omaku <fomaku3@gmail.com>
2023-05-10 14:19:42 +0100

    linear searc

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,21 +5,24 @@
  *@array: pointer to first element
  *@size: number of element in array
  *@value: value to search for
- *Return - -1 if failed
+ *Return: -1 if failed
  */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (array == NULL) {
-		return -1;
+	if (array == NULL)
+	{
+		return (-1);
 	}
-	
-	for (i = 0; i < size; i++) {
+
+	for (i = 0; i < size; i++)
+	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value) {
-			return i;  // Found the value at index i
+		if (array[i] == value)
+		{
+			return (i);  /* Found the value at index i*/
 		}
 	}
-	return -1;
+	return (-1);
 }
